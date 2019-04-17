92cd3503989f1407ccbe2cf989ac0034250294b7
Wan Zhu <wan@Wans-MacBook-Pro.local>
2019-04-17 10:59:36 -0700

    update directory

--- face_cv2.py
+++ face_cv2.py
@@ -11,24 +11,57 @@ eye_cascade = cv2.CascadeClassifier('/Users/wan/dev/opencv/data/haarcascades/haa
 # face_cascade.empty()
 # eye_cascade.empty()
 
-def face_search(directory):
-    file_list = os.listdir(directory)
-    images = glob.glob(os.path.join(directory, '*.jpg'))
+def remove_no_face_search(face_directory):
+    file_list = os.listdir(face_directory)
+    images = glob.glob(os.path.join(face_directory, '*.jpg'))
     number_of_files = len(file_list)
 
     face_image_count = 0
     no_face_count = 0
     for image in images:
-        img = cv2.imread(image)
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+        try:
+            img = cv2.imread(image)
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+        except:
+            print(image)
+            continue
+
         if (len(faces) != 0):
             face_image_count += 1
         else:
-            print('No face found in ' + image)
+#             print('No face found in ' + image)
+            os.remove(os.path.join(image))
             no_face_count += 1
 
-    print('A total of ' + str(number_of_files) + 'was screened by haarcascade face detection.')
+    print('A total of ' + str(number_of_files) + ' was screened by haarcascade face detection.')
+    print(str(face_image_count) + ' of images out of ' + str(number_of_files) + ' that have faces inside was found.')
+    print(str(no_face_count) + ' of images out of ' + str(number_of_files) + ' that have no faces in side was found.')
+
+def remove_face_search(no_face_directory):
+    file_list = os.listdir(no_face_directory)
+    images = glob.glob(os.path.join(no_face_directory, '*.jpg'))
+    number_of_files = len(file_list)
+
+    face_image_count = 0
+    no_face_count = 0
+    for image in images:
+        try:
+            img = cv2.imread(image)
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+        except:
+            print(image)
+            continue
+
+        if (len(faces) != 0):
+            face_image_count += 1
+            os.remove(os.path.join(image))
+        else:
+#             print('No face found in ' + image)
+            no_face_count += 1
+
+    print('A total of ' + str(number_of_files) + ' was screened by haarcascade face detection.')
     print(str(face_image_count) + ' of images out of ' + str(number_of_files) + ' that have faces inside was found.')
     print(str(no_face_count) + ' of images out of ' + str(number_of_files) + ' that have no faces in side was found.')
 
@@ -36,12 +69,18 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
     parser.add_argument(
-        '--directory',
+        '--face_directory',
         type=str,
-        required=True,
+        help='image path to check'
+    )
+
+    parser.add_argument(
+        '--no_face_directory',
+        type=str,
         help='image path to check'
     )
 
     args = parser.parse_args()
 
-    face_search(args.directory)
+    remove_no_face_search(args.face_directory)
+    remove_face_search(args.no_face_directory)
